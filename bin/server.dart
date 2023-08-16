7b2bfb8fde361cbc5927d6b08e495f995d61281e
dushesatwork <guid.empty@gmail.com>
2023-08-16 09:53:57 +0300

    initial server

---
+++ bin/server.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+
+void main(List<String> args) => serve(
+      (request) => Response.ok('Hello, World!\n'),
+      InternetAddress.anyIPv4,
+      8080,
+    );

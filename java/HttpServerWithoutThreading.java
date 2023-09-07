aa810ea35a96a19427f4246c385f5b4697bbc19b
dushesatwork <guid.empty@gmail.com>
2023-09-07 14:55:23 +0300

    initial server - java

---
+++ java/HttpServerWithoutThreading.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class HttpServerWithoutThreading {
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("HTTP сервер запущен на порту " + PORT);
+
+            while (true) {
+                System.out.println("Ожидание входящего соединения...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Входящее соединение принято: " + clientSocket.getInetAddress().getHostAddress());
+
+                handleConnection(clientSocket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleConnection(Socket clientSocket) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            OutputStream outputStream = clientSocket.getOutputStream();
+
+            String line;
+            String uriPath = "";
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                System.out.println(line);
+                if (line.startsWith("GET")) {
+                    uriPath = line.split(" ")[1];
+                }
+            }
+
+            if (uriPath.contains("/fibonacci")) {
+                System.out.println(uriPath);
+                Map params=parse(uriPath);
+                String value=(String)params.get("value");
+
+                String response;
+                int fibonacciResult = fibonacci(Integer.parseInt(value));
+
+                response = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: text/html\r\n\r\n" +
+                        "<html><body>" +
+                        "<h1>Hello, World!</h1>" +
+                        "<p>Число Фибоначчи от 32: " + fibonacciResult + "</p>" +
+                        "</body></html>";
+
+                outputStream.write(response.getBytes("UTF-8"));
+            } else {
+                String response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                outputStream.write(response.getBytes("UTF-8"));
+            }
+
+            outputStream.close();
+            reader.close();
+            clientSocket.close();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static HashMap<String, String> parse(String url) throws URISyntaxException {
+        HashMap<String, String> params = new HashMap<>();
+        URI uri = new URI(url);
+        String query = uri.getQuery();
+        if (query != null) {
+            String[] pairs = query.split("&");
+            for (String pair : pairs) {
+                int idx = pair.indexOf("=");
+                String key = idx > 0 ? pair.substring(0, idx) : pair;
+                String value = idx > 0 && pair.length() > idx + 1 ? pair.substring(idx + 1) : null;
+                params.put(key, value);
+            }
+        }
+        return params;
+    }
+
+    private static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}

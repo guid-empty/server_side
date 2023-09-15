8c348e8bbb3fcc43ded114a9f38f3c0ae295ce77
dushesatwork <guid.empty@gmail.com>
2023-09-15 10:59:56 +0300

    initial server - java

--- bin/11_shelf_router_server.dart
+++ bin/11_shelf_router_server.dart
@@ -9,19 +9,27 @@ import 'package:shelf_router/shelf_router.dart';
 /// http://localhost:8080/json
 /// http://localhost:8080/echo/somemessage
 /// http://localhost:8080/delete/<id>
+/// http://localhost:8080/create/<name>
 /// http://localhost:8080/echo/somemessage/error
 ///
 Future<void> main() async {
   final router = Router()
     ..get('/json', _jsonHandler)
     ..delete('/delete/<id>', _deleteEntityHandler)
+    ..put('/create/<name>', _createEntityHandler)
     ..get('/echo/<message>', _echoHandler);
 
   await serve(router, InternetAddress.anyIPv4, 8080);
 }
 
+Future<Response> _createEntityHandler(Request request) async {
+  final message = request.params['name'];
+
+  return Response.ok('$message\n');
+}
+
 Future<Response> _deleteEntityHandler(Request request) async {
-  final message = request.params['message'];
+  final message = request.params['id'];
 
   return Response.ok('$message\n');
 }
